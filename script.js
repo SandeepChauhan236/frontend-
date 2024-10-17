0cf97d2beaef640b2140d9a3572360efc8a7e7ba
Sandeep Chauhan <sandeepchauhan2326@gmail.com>
2024-10-17 10:22:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// const product = {
+//     tittle: "ball pen",
+//     rating:4,
+//     offer:5,
+//     price:270,
+// };
+
+// console.log(product);
+
+const profile = {
+    name:"sandeep",
+    isfollow: true,
+    followers: 378,
+    following: 171,
+};
+
+console.log(profile);
